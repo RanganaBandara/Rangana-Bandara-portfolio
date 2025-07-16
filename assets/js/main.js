0de8125e7df1fd4ae02989c32a15a8f1678d73f1
Your Name <you@example.com>
2025-07-17 04:54:06 +0530

    new_portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,14 +12,6 @@
   /**
    * Header toggle
    */
-  const headerToggleBtn = document.querySelector('.header-toggle');
-
-  function headerToggle() {
-    document.querySelector('#header').classList.toggle('header-show');
-    headerToggleBtn.classList.toggle('bi-list');
-    headerToggleBtn.classList.toggle('bi-x');
-  }
-  headerToggleBtn.addEventListener('click', headerToggle);
 
   /**
    * Hide mobile nav on same-page/hash links
